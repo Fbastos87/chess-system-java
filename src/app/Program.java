27727b60d92f95ded01ad9912be1bda44a6e1402
Felipe Bastos <fbastos87@gmail.com>
2022-09-30 11:43:31 -0300

    Criado classe Position

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,9 +1,13 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
+		Position posicao = new Position(1,1);
+		
+		System.out.println(posicao);
 
 	}
 
